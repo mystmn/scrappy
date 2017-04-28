2b37a2271178d7bb0f9251672e43d280336e0738
pcameron <pcameron5@gmail.com>
2017-04-27 21:23:29 -0400

    working and clean

--- myspider.py
+++ myspider.py
@@ -7,25 +7,22 @@ class BlogSpider(scrapy.Spider):
 
     def parse(self, response):
         x = []
-        itemHeader = {
+        item_list = {
             'card_name': '',
             'price': '',
             'qty': '',
         }
 
-        for title in response.css('div.sellerContainer'):
+        for each in response.css('div.sellerContainer'):
 
-            itemHeader['card_name'] = title.css('a ::text').extract_first()
+            item_list['card_name'] = each.css('a ::text').extract_first()
 
-            itemHeader['price'] = title.css('span.pricegreen::text').extract()
+            item_list['price'] = each.css('span.pricegreen::text').extract()
 
-#            itemHeader['qty'] = title.css('span.smalltext').extract()
+#            item_list['qty'] = each.css('span.smalltext').extract()
 
-            x.append(itemHeader['card_name'])
-            x.append(itemHeader['price'])
-#            x.append(itemHeader['qty'])
+            x.append(item_list['card_name'])
+            x.append(item_list['price'])
+#            x.append(item_list['qty'])
 
         print(x)
-
-# for price in response.css('span.pricegreen'):
-#            yield {'price': price.css('::text').extract_first()}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+scrapy runspider myspider.py
+
